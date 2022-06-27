4800ca8aec81fbc5e2caf62457d926aa7ce17f09
Morev Ilya <ilya.morev@aalto.fi>
2022-06-27 13:17:06 +0300

    simple-pi solution

---
+++ mpi/simple-pi/main.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cmath>
+#include <mpi.h>
+
+constexpr int n = 840;
+
+int main(int argc, char** argv)
+{
+
+  printf("Computing approximation to pi with N=%d\n", n);
+  
+  int myid;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  
+
+  int istart = (myid==0)?1:n/2+1;
+  int istop = (myid==0)?n/2:n;
+
+  double pi = 0.0;
+  for (int i=istart; i <= istop; i++) {
+    double x = (i - 0.5) / n;
+    pi += 1.0 / (1.0 + x*x);
+  }
+  
+  int tag = 1, msgsize = 1;
+  if (myid == 0) {
+    double other_part[1];
+    MPI_Recv(other_part, msgsize, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    pi += other_part[0]; 
+    pi *= 4.0 / n;
+    printf("Approximate pi=%18.16f (exact pi=%10.8f)\n", pi, M_PI);
+  } else if (myid == 1) {
+    double send_arr[] = {pi};
+    MPI_Send(send_arr, msgsize, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+  return 0;
+
+}
+   
+
