bb235f2681ed247ba8c91a8ca1f9114360ed2cc5
Morev Ilya <ilya.morev@aalto.fi>
2022-06-30 11:36:48 +0300

    mandelbrot solution

--- hybrid/mandelbrot/cpp/mandelbrot.cpp
+++ hybrid/mandelbrot/cpp/mandelbrot.cpp
@@ -79,6 +79,7 @@ void mandelbrot_block(int *iter_counts, int w, int h, complex<double> cmin,
         // Subdivide recursively
         for (int i = 0; i < SUBDIV; i++) {
             for (int j = 0; j < SUBDIV; j++) {
+                #pragma omp task firstprivate(block_size)
                 mandelbrot_block(iter_counts, w, h, cmin, cmax,
                                  x0 + i * block_size, y0 + j * block_size,
                                  d / SUBDIV, depth + 1);
@@ -112,7 +113,8 @@ int main(int argc, char **argv)
 
 // TODO create parallel region. How many threads should be calling
 // mandelbrot_block in this uppermost level?
-
+    #pragma omp parallel
+    #pragma omp single
     {
         mandelbrot_block(iter_counts, w, h, cmin, cmax,
                          0, 0, w, 1);
@@ -130,4 +132,3 @@ int main(int argc, char **argv)
     free(iter_counts);
     return 0;
 }
-
