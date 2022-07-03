905ad8523d6e38dec4fafac10e2ba00e9178985e
Ilya Morev <morev.ilya@gmail.com>
2022-07-03 11:48:45 +0300

    gpu-hip kernel-saxpy

--- gpu-hip/kernel-saxpy/saxpy.cpp
+++ gpu-hip/kernel-saxpy/saxpy.cpp
@@ -2,7 +2,22 @@
 #include <stdio.h>
 #include <math.h>
 
+#define HIP_SAFECALL(x) {      \
+  hipError_t status = x;       \
+  if (status != hipSuccess) {  \
+    printf("HIP Error: %s\n", hipGetErrorString(status));  \
+  } }
+
 // TODO: add a device kernel that calculates y = a * x + y
+__global__ void kernel_(int n, float a, float *x, float *y)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+
+    for (; tid < n; tid += stride) {
+        y[tid] = a*x[tid] + y[tid];
+    }
+}
 
 int main(void)
 {
@@ -20,13 +35,25 @@ int main(void)
     }
 
     // TODO: allocate vectors x_ and y_ on the GPU
+    HIP_SAFECALL(hipMalloc(&x_, sizeof(float) * n));
+    HIP_SAFECALL(hipMalloc(&y_, sizeof(float) * n));
+
     // TODO: copy initial values from CPU to GPU (x -> x_ and y -> y_)
+    HIP_SAFECALL(hipMemcpy(x_, x, sizeof(float) * n, hipMemcpyHostToDevice));
+    HIP_SAFECALL(hipMemcpy(y_, y, sizeof(float) * n, hipMemcpyHostToDevice));
+
 
     // TODO: define grid dimensions
+    dim3 blocks(32);
+    dim3 threads(256);
     // TODO: launch the device kernel
-    hipLaunchKernelGGL(...);
+    //HIP_SAFECALL(hipLaunchKernelGGL(kernel_, blocks, threads, 0, 0, n, a, x_, y_));
+    hipLaunchKernelGGL(kernel_, blocks, threads, 0, 0, n, a, x_, y_);
+    HIP_SAFECALL(hipGetLastError()); 
 
     // TODO: copy results back to CPU (y_ -> y)
+    HIP_SAFECALL(hipMemcpy(y, y_, sizeof(float) * n, hipMemcpyDeviceToHost));
+
 
     // confirm that results are correct
     float error = 0.0;
