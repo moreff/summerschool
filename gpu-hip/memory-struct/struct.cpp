f99cddd02ce67d8ffb476cc6472a88c07e5431fd
Ilya Morev <morev.ilya@gmail.com>
2022-07-04 11:15:40 +0300

    gpu-hip memory-struct

--- gpu-hip/memory-struct/struct.cpp
+++ gpu-hip/memory-struct/struct.cpp
@@ -57,9 +57,12 @@ void runHost()
 /* Run on device using Unified Memory */
 void runDeviceUnifiedMem()
 {
-  #error Allocate struct using Unified Memory
+  Example *ex; 
+  hipMallocManaged(&ex, sizeof(Example));
+  ex->size = 10;
 
-  #error Allocate struct members using Unified Memory
+  hipMallocManaged((void**) &ex->x, ex->size*sizeof(float));
+  hipMallocManaged((void**) &ex->idx, ex->size*sizeof(int));
 
   // Initialize struct from host
   for(int i = 0; i < ex->size; i++)
@@ -68,42 +71,73 @@ void runDeviceUnifiedMem()
     ex->idx[i] = i;
   }
 
-  #error Print struct values from device by calling hipKernel()
+  // #error Print struct values from device by calling hipKernel()
   printf("\nDevice (UnifiedMem):\n");
-
-  #error Free struct
+  int gridsize = (ex->size - 1 + BLOCKSIZE) / BLOCKSIZE;
+  hipLaunchKernelGGL(hipKernel, gridsize, BLOCKSIZE, 0, 0, ex);
+//  hipStreamSynchronize(0);
+  
+  hipFree(ex->x);
+  hipFree(ex->idx);
+  hipFree(ex);
 }
 
 /* Create the device struct (needed for explicit memory management) */
 Example* createDeviceExample(Example *ex)
 {
-  #error Allocate device struct
+  Example* d_ex;
+  // #error Allocate device struct
+  hipMalloc((void **)&d_ex, sizeof(Example));
+  
+  float *d_x;
+  int *d_idx;
 
-  #error Allocate device struct members
+  // #error Allocate device struct members
+  hipMalloc((void **) &(d_x), ex->size*sizeof(float));
+  hipMalloc((void **) &(d_idx), ex->size*sizeof(int));
 
-  #error Copy arrays pointed by the struct members from host to device
+  // #error Copy arrays pointed by the struct members from host to device
+  hipMemcpy(d_x, ex->x, ex->size*sizeof(float), hipMemcpyHostToDevice);
+  hipMemcpy(d_idx, ex->idx, ex->size*sizeof(float), hipMemcpyHostToDevice);
 
-  #error Copy struct members from host to device
-
-  #error Return device struct
+  // #error Copy struct members from host to device
+  hipMemcpy(&(d_ex->x), &d_x, sizeof(float*), hipMemcpyHostToDevice);
+  hipMemcpy(&(d_ex->idx), &d_idx, sizeof(int*), hipMemcpyHostToDevice);
+  hipMemcpy(&(d_ex->size), &(ex->size), sizeof(int), hipMemcpyHostToDevice);
+  
+  // #error Return device struct
+  return d_ex;
 }
 
 /* Free the device struct (needed for explicit memory management) */
 void freeDeviceExample(Example *d_ex)
 {
-  #error Copy struct members (pointers) from device to host
+  float *d_x;
+  int *d_idx;
+  // #error Copy struct members (pointers) from device to host
+  hipMemcpy(&d_x, &(d_ex->x), sizeof(float*), hipMemcpyDeviceToHost);
+  hipMemcpy(&d_idx, &(d_ex->idx), sizeof(int*), hipMemcpyHostToDevice);
+  
+  // #error Free device struct members
+  hipFree(d_x);
+  hipFree(d_idx);
 
-  #error Free device struct members
-
-  #error Free device struct
+  // #error Free device struct
+  hipFree(d_ex);
 }
 
 /* Run on device using Explicit memory management */
 void runDeviceExplicitMem()
 {
-  #error Allocate host struct
+  // #error Allocate host struct
+  Example *ex;
+  hipHostMalloc((void **) &ex, sizeof(Example));
+  ex->size = 10;
 
-  #error Allocate host struct members
+  // #error Allocate host struct members
+  hipHostMalloc((void**) &(ex->x), ex->size*sizeof(float));
+  hipHostMalloc((void**) &(ex->idx), ex->size*sizeof(int));
+
 
   // Initialize host struct
   for(int i = 0; i < ex->size; i++)
@@ -115,13 +149,22 @@ void runDeviceExplicitMem()
   // Allocate device struct and copy values from host to device
   Example *d_ex = createDeviceExample(ex);
 
-  #error Print struct values from device by calling hipKernel()
+
+  // #error Print struct values from device by calling hipKernel()
   printf("\nDevice (ExplicitMem):\n");
+  const int gridsize = (ex->size - 1 + BLOCKSIZE) / BLOCKSIZE;
+  hipLaunchKernelGGL(hipKernel,
+    gridsize, BLOCKSIZE, 0, 0,
+    d_ex);
+  hipStreamSynchronize(0);
 
   // Free device struct
   freeDeviceExample(d_ex);
 
-  #error Free host struct
+  // #error Free host struct
+  hipHostFree(ex->x);
+  hipHostFree(ex->idx);
+  hipHostFree(ex);
 }
 
 /* The main function */
